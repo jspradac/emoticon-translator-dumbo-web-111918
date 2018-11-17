e42d9dcb120c74d901281ddd8df5c5340b1f2556
Samuel Prada <jspradac@gmail.com>
2018-11-17 03:28:05 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,11 @@ require "yaml"
 
 def load_library(file_path)
   gov = YAML.load_file(file_path)
-  emoticons = {"get_emoticon" => {}, "get_meaning" => {}} 
-  gov.each do |
+  emo_hash = {"get_emoticon" => {}, "get_meaning" => {}} 
+  gov.each do |meaning, emoticons|
+    emoticons.each do |emoticon|
+      emo_hash["get_meaning"][emoticon] = "#{meaning}"
+      emo_hash[]
 end
 
 def get_japanese_emoticon
