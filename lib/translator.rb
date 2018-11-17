420d6012d7d4fc938c733505a31189ee6591335f
Samuel Prada <jspradac@gmail.com>
2018-11-17 03:51:03 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,8 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  gov = load_library(file_path)
+  emo_hash = load_library(file_path)
+  emo_hash[emoticon]
 end
 
 def get_english_meaning
